6a91e68305c8f56211b9216346399ef5e3ef0c62
zjy <2910553808@qq.com>
2022-05-23 11:24:34 +0800

    修改提交

--- Server/src/ServerSrc/main.cc
+++ Server/src/ServerSrc/main.cc
@@ -1,9 +1,11 @@
+#include <cstdio>
 #include <drogon/drogon.h>
 int main() {
+  printf("启动服务器");
     //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0",80);
+   // drogon::app().addListener("0.0.0.0",8080);
     //Load config file
-    //drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile("../config.json");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
     return 0;
