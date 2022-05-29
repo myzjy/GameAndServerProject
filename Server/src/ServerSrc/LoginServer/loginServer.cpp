839b880a2e955976215977a2c8a1788d3b088e99
zjy <2910553808@qq.com>
2022-05-29 17:20:00 +0800

    修改

---
+++ Server/src/ServerSrc/LoginServer/loginServer.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Apple on 2022/5/25.
+//
+#define DROGON_TEST_MAIN
+
+#include <drogon/drogon.h>
+int main(){
+    drogon::app().loadConfigFile("../config.json");
+    //Run HTTP framework,the method will block in the internal event loop
+    drogon::app().run();
+    return 0;
+}
