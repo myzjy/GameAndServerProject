5f5a02ef84fe7fe0b266368e2310016a9899945f
zjy <2910553808@qq.com>
2022-05-30 22:29:40 +0800

    修改头文件

--- Server/src/ServerSrc/main.cc
+++ Server/src/ServerSrc/main.cc
@@ -2,11 +2,7 @@
 #include <drogon/HttpAppFramework.h>
 #include <drogon/HttpResponse.h>
 #include <drogon/drogon.h>
-#include <drogon/utils/FunctionTraits.h>
-#include <functional>
-#include <jsoncpp/json/json.h>
-#include <jsoncpp/json/value.h>
-#include <string>
+
 int main() {
   printf("启动服务器");
   // Set HTTP listener address and port
